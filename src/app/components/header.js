293041e73be9a3bdf6278766fec45cc0b5a7669e
AurelienChetot <aurelien.chetot@gmail.com>
2024-11-06 12:28:45 +0100

    add position

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -15,19 +15,18 @@ export default function Header() {
 
   return (
     <header>
-      <div className="gite-images">
-        {gites.map((gite) => (
-          <div key={gite.id} className="gite-card">
-            <Image
-              src={gite.image}
-              alt={gite.title}
-              width={1920}
-              height={1080}
-              layout="responsive"
-            />
-          </div>
-        ))}
-      </div>
+      {gites.map((gite) => (
+        <div key={gite.id} className="gite-header">
+          <Image
+            className="img-header"
+            src={gite.image}
+            alt={gite.title}
+            width={1920}
+            height={1080}
+            layout="responsive"
+          />
+        </div>
+      ))}
     </header>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,12 @@ import Header from "./components/header";
 
 // Server-side data fetching
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/data.json"); // Assurez-vous que data.json est dans public/
-  const gites = await res.json(); // Charger les données
+  const res = await fetch("http://localhost:3000/data.json");
+  const gites = await res.json();
 
   return (
     <div>
-      <Header gites={gites} /> {/* Passer les données à Header */}
+      <Header gites={gites} />
     </div>
   );
 }
