c063504b6e82d9680a353124f492ea0bcbbce7b3
AurelienChetot <aurelien.chetot@gmail.com>
2024-11-06 13:33:37 +0100

    add text and style for header

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -3,6 +3,9 @@
 
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 export default function Header() {
   const [gites, setGites] = useState([]);
@@ -13,20 +16,37 @@ export default function Header() {
       .then((data) => setGites(data));
   }, []);
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <header>
-      {gites.map((gite) => (
-        <div key={gite.id} className="gite-header">
-          <Image
-            className="img-header"
-            src={gite.image}
-            alt={gite.title}
-            width={1920}
-            height={1080}
-            layout="responsive"
-          />
-        </div>
-      ))}
+      <Slider {...settings}>
+        {gites.map((gite) => (
+          <div key={gite.id} className="gite-header">
+            <Image
+              className="img-header"
+              src={gite.image}
+              alt={gite.title}
+              width={1920}
+              height={1080}
+            />
+            <div className="text-container">
+              <h1>
+                Détente, Nature, <br /> Partage
+              </h1>
+              <h2>{gite.title}</h2>
+            </div>
+          </div>
+        ))}
+      </Slider>
     </header>
   );
 }
