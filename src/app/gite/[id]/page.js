c413d47dc6aec0a3d812c14ad27afa214c3bc986
AurelienChetot <aurelien.chetot@gmail.com>
2024-11-09 20:58:03 +0100

    change name next public data url

--- src/app/gite/[id]/page.js
+++ src/app/gite/[id]/page.js
@@ -16,7 +16,7 @@ export default async function GitePage({ params }) {
   // stock id gite dans params
   const { id } = await params;
 
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL);
+  const res = await fetch(process.env.NEXT_PUBLIC_DATA_URL);
   const gites = await res.json();
 
   // Filtrer les données pour l'id du gite
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import HomeClient from "./components/homeclient";
 
 export default async function Home() {
   // Récupere le fetch coté serveur (non client)
-  const res = await fetch(process.env.NEXT_PUBLIC_API_URL);
+  const res = await fetch(process.env.NEXT_PUBLIC_DATA_URL);
   const gites = await res.json();
 
   return (
